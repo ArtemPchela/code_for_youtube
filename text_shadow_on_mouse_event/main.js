9b5410408cd5a484ccdee5c92f94069ef74725a8
ArtemPchela <pchelenkovartem@gmail.com>
2024-06-12 15:32:21 +0200

    made a changes in "Text Shadow on Mouse Event JavaScript/HTML/CSS." now we have two variants

--- text_shadow_on_mouse_event/main.js
+++ text_shadow_on_mouse_event/main.js
@@ -8,8 +8,38 @@ document.addEventListener("DOMContentLoaded", function () {
     const offsetY = e.clientY - (rect.top + rect.height / 2);
 
     textElement.style.textShadow = `
-      ${offsetX / -20}px ${offsetY / -20}px 5px rgba(24, 143, 167, 0.5),
-      ${offsetX / 12}px ${offsetY / 12}px 5px rgba(218, 44, 56, 0.3)
+      ${offsetX / 12}px ${offsetY / 12}px 5px #EA9E8D,
+      ${offsetX / 6}px ${offsetY / 6}px 5px #DAEFB3,
+      ${offsetX / 12}px ${offsetY / -12}px 5px #84ACCE,
+      ${offsetX / -6}px ${offsetY / -6}px 5px #C5AFA4
     `;
   })
 })
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const textElements = document.querySelectorAll(".text"); // Select all elements with class 'viral-text'
+//   const maxShadowDistance = 50; // Maximum distance the shadow can move in pixels
+//
+//   window.addEventListener("mousemove", function (e) {
+//     textElements.forEach((item) => {
+//       const rect = item.getBoundingClientRect();
+//       let offsetX = e.clientX - (rect.left + rect.width / 2); // X position relative to element center
+//       let offsetY = e.clientY - (rect.top + rect.height / 2); // Y position relative to element center
+//
+//       offsetX = Math.min(
+//         Math.max(offsetX / 10, -maxShadowDistance),
+//         maxShadowDistance,
+//       );
+//       offsetY = Math.min(
+//         Math.max(offsetY / 10, -maxShadowDistance),
+//         maxShadowDistance,
+//       );
+//
+//       item.style.textShadow = `
+//                 ${offsetX}px ${offsetY}px 5px rgba(0,0,0,0.3),
+//                 ${offsetX * 1.2}px ${offsetY * 1.2}px 10px rgba(0,0,0,0.5),
+//                 ${offsetX * 4}px ${offsetY * 4}px 15px rgba(0,0,0,0.5)
+//             `;
+//     });
+//   });
+// });
